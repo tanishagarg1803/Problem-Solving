bf9bc3b304dbb02fec6bfd5b8effbc1c8f158d06
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2021-10-12 23:24:29 +0530

    Create 1538A-StoneGame.cpp

---
+++ codeforces/1538A-StoneGame.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for (int &e : v){
+            cin >> e;
+        }
+        int maxPos = max_element(v.begin(), v.end())-v.begin();
+        int minPos = min_element(v.begin(), v.end())-v.begin();
+        cout<<min({max(maxPos, minPos)+1, n-min(maxPos, minPos), n-maxPos+minPos+1, n-minPos+maxPos+1})<<endl;
+    }
+    return 0;
+}

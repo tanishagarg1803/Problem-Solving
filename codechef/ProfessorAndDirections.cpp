a0d12b28de36244c4a86b19e0c20b3ef92dc5717
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2021-10-23 08:54:12 +0530

    Create ProfessorAndDirections.cpp

---
+++ codechef/ProfessorAndDirections.cpp
@@ -0,0 +1,38 @@
+// Problem Link :https://www.codechef.com/START16C/problems/DIRECTN
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+     ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int c=0;
+        for(int i=1;s[i]!='\0';i++){
+            if(s[i]=='L' && s[i-1]=='L'){
+                c=1;
+                break;
+            }
+            else if(s[i]=='R' && s[i-1]=='R'){
+                c=1;
+                break;
+            }
+        }
+        if(c==1){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+    
+	// your code goes here
+	return 0;
+}

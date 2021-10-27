53ce6cfa4fdd43f3899c3204edf4fb8bfbbeae36
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2021-10-27 22:12:17 +0530

    Create 734A-AntonAndDanik.cpp

---
+++ codeforces/734A-AntonAndDanik.cpp
@@ -0,0 +1,27 @@
+// problem link: https://codeforces.com/contest/734/problem/A
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n,a=0,d=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(int i=0;i<n;i++){
+        if(s[i]=='A'){
+            a++;
+        }
+        else{
+            d++;
+        }
+    }
+    if(a>d){
+        cout<<"Anton";
+    }
+    else if(a==d){
+        cout<<"Friendship";
+    }
+    else{
+        cout<<"Danik";
+    }
+}

d9c8fcb698885e517df43db84d3f2396aae6d4c0
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2021-10-30 19:16:15 +0530

    Create 281A-WordCapitalization.cpp

---
+++ codeforces/281A-WordCapitalization.cpp
@@ -0,0 +1,14 @@
+// problem link: https://codeforces.com/contest/281/problem/A
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    if(s[0]>=97&& s[0]<=122){
+        s[0]=s[0]-32;
+    }
+    cout<<s;
+    return 0;
+}

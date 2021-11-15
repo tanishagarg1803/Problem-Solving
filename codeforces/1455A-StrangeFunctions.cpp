59b0708b93f8360772ab882cc2368f0079927bb8
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2021-11-15 22:01:53 +0530

    Create 1455A-StrangeFunctions.cpp

---
+++ codeforces/1455A-StrangeFunctions.cpp
@@ -0,0 +1,17 @@
+// Problem Link:https://codeforces.com/contest/1455/problem/A
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   long long int t;
+    cin>>t;
+    while(t--){
+        string s;
+        int c;
+        cin>>s;
+        c=s.size();
+        cout<<c<<endl;
+    }
+    return 0;
+}

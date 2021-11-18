521f7536f8624a350d5399a95ddb5287bc0aa5be
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2021-11-18 20:54:01 +0530

    Create 1553A-DigitsSum.cpp

---
+++ codeforces/1553A-DigitsSum.cpp
@@ -0,0 +1,17 @@
+// Problem Link: https://codeforces.com/contest/1553/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+     ll t;
+     cin>> t;
+     while(t--){
+         ll n;
+         cin>>n;
+         cout<<(n+1)/10<<endl;
+     }
+	// your code goes here
+	return 0;
+}

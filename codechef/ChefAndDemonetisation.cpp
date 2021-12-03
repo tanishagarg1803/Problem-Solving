6d7327b7a83a580887242b1a1abf8be005fdc286
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2021-12-03 10:20:15 +0530

    Create ChefAndDemonetisation.cpp

---
+++ codechef/ChefAndDemonetisation.cpp
@@ -0,0 +1,35 @@
+// Problem Link: https://www.codechef.com/LTIME85B/problems/CHFMOT18
+
+#include <stdio.h>
+
+int main(void) {
+    int t,s,n,a,b,c;
+    scanf("%d",&t);
+    while(t>0)
+    {
+        t--;
+        a=0,b=0,c=0;
+        scanf("%d%d",&s,&n);
+        a=s/n;
+        b=s%n;
+           if(b!=0){
+            if(b%2==0)
+            {
+               c=1;
+            }
+            else if (b==1)
+            {
+               c=1;
+            }
+            else {
+                c=2;
+            }
+          }
+
+
+        printf("%d\n",a+c);
+
+    }
+	// your code goes here
+	return 0;
+}

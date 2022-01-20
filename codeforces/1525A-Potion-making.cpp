4e2e6066b8d59ec6f1dedfdc98c37c8f8dff6345
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2022-01-20 10:48:28 +0530

    Create 1525A-Potion-making.cpp

---
+++ codeforces/1525A-Potion-making.cpp
@@ -0,0 +1,18 @@
+// Problem Link: https://codeforces.com/contest/1525/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int k;
+        cin>>k;
+        int c=100/__gcd(k,100);
+        cout<<c<<endl;
+        
+        }
+	// your code goes here
+	return 0;
+}

663c602dbcafae68d3fd3580305f21f6537f926a
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2022-02-28 12:36:54 +0530

    Create merge-two-sorted-list.cpp

---
+++ Leetccode/merge-two-sorted-list.cpp
@@ -0,0 +1,26 @@
+// Problem Link: https://leetcode.com/problems/merge-two-sorted-lists/
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL) 
+          return l2;
+        if(l2==NULL)
+            return l1;
+        ListNode* res;
+        if(l1->val > l2->val){
+            swap(l1,l2);
+        }
+        res=l1;
+        while(l1!=NULL && l2!=NULL){
+             ListNode* temp=NULL;
+            while(l1 && l1->val<=l2->val){
+                temp=l1;
+                l1=l1->next;
+            }
+            temp->next=l2;
+            swap(l1,l2);
+        }
+        return res;
+    }
+};

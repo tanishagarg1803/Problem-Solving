6b922ee5d2604034439e50a50e637e2edc02a387
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2022-04-06 09:59:44 +0530

    Create 1399A-RemoveSmallest.cpp

---
+++ codeforces/1399A-RemoveSmallest.cpp
@@ -0,0 +1,31 @@
+// Problem Link: https://codeforces.com/contest/1399/problem/A
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,c=0;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a,a+n);
+        for(int i=1;i<n;i++){
+            if(a[i]-a[i-1]>1){
+                c=1;
+                break;
+            }
+        }
+        if(c==1)
+        cout<<"NO"<<endl;
+        else
+        cout<<"YES"<<endl;
+    }
+	// your code goes here
+	return 0;
+}

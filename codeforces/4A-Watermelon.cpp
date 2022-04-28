581f5b1a1ef6f21b1ffcbb6649aa5fc48e4ee63c
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2022-04-28 11:51:00 +0530

    Create 4A-Watermelon.cpp

---
+++ codeforces/4A-Watermelon.cpp
@@ -0,0 +1,16 @@
+// Problem Link: https://codeforces.com/contest/4/problem/A
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0&&n!=2)
+    {
+        cout<<"YES";
+        
+    }
+    else{
+        cout<<"NO";
+    }
+}

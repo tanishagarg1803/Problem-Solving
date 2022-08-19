e8496989321bd5261be9c1e474fdcbe4b92f7cf9
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2022-08-19 18:36:32 +0530

    Create find-smallest-letter-greater-than-target.cpp

---
+++ Leetccode/find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,18 @@
+// Problem Link: https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int s=0,e=letters.size()-1;
+        while(s<=e){
+            int  mid=s+(e-s)/2;
+            if(target<letters[mid]){
+                e=mid-1;
+            }
+            else {
+                s=mid+1;
+            }
+        }
+        return letters[s%letters.size()];
+    }
+};

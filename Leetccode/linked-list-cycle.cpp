053e54de333292de2a9cb4ca83412cdcb5c4e2a1
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2022-09-05 13:26:40 +0530

    Create linked-list-cycle.cpp

---
+++ Leetccode/linked-list-cycle.cpp
@@ -0,0 +1,18 @@
+// Problem Link: https://leetcode.com/problems/linked-list-cycle/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow=head,*fast=head;
+        if(head == NULL)
+            return false;
+        while(fast->next!=NULL&&fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};

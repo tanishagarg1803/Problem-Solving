60cb6441a99c70b9bc13c52d91ed4e0fab0cf482
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2022-11-09 13:48:33 +0530

    Create image-overlap.cpp

---
+++ Leetccode/image-overlap.cpp
@@ -0,0 +1,25 @@
+// Problem Link: https://leetcode.com/problems/image-overlap/
+
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<vector<int>> va, vb;
+        int rows = A.size(), cols = A[0].size();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (A[i][j]) va.push_back({i, j});
+                if (B[i][j]) vb.push_back({i, j});
+            }
+        }
+        unordered_map<string, int> mp;
+        for (auto &pa : va) {
+            for (auto &pb : vb) {
+                string s = to_string(pa[0] - pb[0]) + " " + to_string(pa[1] - pb[1]);
+                mp[s]++;
+            }
+        }
+        int res = 0;
+        for (auto &it : mp) res = max(res, it.second);
+        return res;
+    }
+};

0335f0e28dd1a543916d2c965e28222dd12e371a
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2022-11-13 12:04:33 +0530

    Create longest-palindrome-by-concatenating-two-letter-words.cpp

---
+++ Leetccode/longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,33 @@
+// Problem Link: https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/
+
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+    unordered_map<string, int> m;
+    int unpaired = 0, ans = 0;
+    for (string w: words) {
+        if (w[0] == w[1]) {
+            if (m[w] > 0) {
+                unpaired--;
+                m[w]--;
+                ans += 4;
+            }
+            else {
+                m[w]++;
+                unpaired++;
+            }
+        }
+        else {
+            string rev = w;
+            reverse(rev.begin(), rev.end());
+            if (m[rev] > 0) {
+                ans += 4;
+                m[rev]--;
+            }
+            else m[w]++;
+        }
+    }
+    if (unpaired > 0) ans += 2;
+    return ans;
+}
+};

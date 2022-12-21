c3d564c9549eaecf3019db57d113e02df6d31749
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2022-12-21 23:21:37 +0530

    Create climbing-stairs.cpp

---
+++ Leetccode/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+// Problem Link: https://leetcode.com/problems/climbing-stairs/description/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev1=1,prev2=1;
+        for(int i=2;i<=n;i++){
+            int curi=prev1+prev2;
+            prev2=prev1;
+            prev1=curi;
+        }
+        return prev1;
+    }
+};

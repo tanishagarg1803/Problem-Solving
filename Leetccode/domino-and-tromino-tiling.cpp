2e0c0b2b67f668133f451f5dcf41ff9ff4011617
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2023-01-14 11:43:05 +0530

    Create domino-and-tromino-tiling.cpp

---
+++ Leetccode/domino-and-tromino-tiling.cpp
@@ -0,0 +1,15 @@
+// Problem Link: https://leetcode.com/problems/domino-and-tromino-tiling/
+
+class Solution {
+public:
+        int numTilings(int N) {
+        int a = 0, b = 1, c = 1, c2, mod = 1e9 + 7;
+        while (--N) {
+            c2 = (c * 2 % mod + a) % mod;
+            a = b;
+            b = c;
+            c = c2;
+        }
+        return c;
+    }
+};

1d26a3b4a968db00cb1de8489dfc083dda32ae93
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2023-01-31 21:31:49 +0530

    Create minimum-number-of-arrows-to-burst-balloons.cpp

---
+++ Leetccode/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,18 @@
+// Problem Link: https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& p) {
+        sort(p.begin(), p.end());
+        int lastpoint = p[0][1];
+        int ans = 1;
+        for(auto point : p) {
+            if(point[0] > lastpoint) {
+                ans++;
+                lastpoint = point[1];
+            }
+            lastpoint = min(point[1],lastpoint);
+        }
+        return ans;
+    }
+};
